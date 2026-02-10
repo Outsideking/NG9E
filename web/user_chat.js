506636b1d577592c03fa1df7efde2851e05b6f9a
Rufio Dinoto(Musk) <rufiodinoto244@gmail.com>
2026-02-10 21:19:25 +0700

    Update user_chat.js

--- web/user_chat.js
+++ web/user_chat.js
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+  <title>NG9E Chat</title>
+</head>
+<body>
+  <h1>NG9E Chat</h1>
+  <div id="chat"></div>
+  <input type="text" id="message" placeholder="Say something...">
+  <button onclick="sendMessage()">Send</button>
+  <script src="user_chat.js"></script>
+</body>
+</html>
